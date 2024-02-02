25e5b6e9ffd7edaf9fd191df7b4787a79230627d
KrystianSzynkiewicz <krystianszynkiewicz@gmail.com>
2024-02-02 12:48:46 +0100

    add the whole project form 2022 (about 2 months of learning HTML and CSS

---
+++ stickyheader.js
@@ -0,0 +1,30 @@
+//STICKYHEADER
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+//SIDE BAR//
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
